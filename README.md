753c5ee2b82b0bdf3dd4c08ae4b55a3b1dc9bc1a
Gabe <matador.gabe@gmail.com>
2016-06-18 00:34:58 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# ap_mobile_api
+
+a [Sails](http://sailsjs.org) application
---
+++ api/models/Profile.js
@@ -0,0 +1,45 @@
+/**
+ * Profile.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: 
+  {
+
+  	userid: 
+  	{
+  		model: 'user'
+  	},
+
+  	firstname: 
+  	{
+  		type: 'string'
+  	},
+
+  	lastname: 
+  	{
+  		type: 'string'
+  	},
+
+  	gender: 
+  	{
+  		type: 'string'
+  	}, 
+
+  	dob: 
+  	{
+  		type: 'text'
+  	},
+
+  	address:
+  	{
+  		type: 'text'
+  	}
+
+  }
+};
+
